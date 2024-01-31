58c72551d83ef9174e22dd27eeff2b16700a2de9
stephanieparra <stephanieparrastudio@gmail.com>
2024-01-30 16:38:56 -0800

    Created dropdown list to change number of profiles displayed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 const randomFolks = document.querySelector(".random-peeps");
 const selectUserNumber = document.querySelector("#users");
 
-const getData = async function () {
-  const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function (numUsers) {
+  const usersRequest = await fetch(
+    `https://randomuser.me/api?results=${numUsers}`
+  );
   const data = await usersRequest.json();
   //console.log(data);
   const userResults = data.results;
   //console.log(userResults);
   displayUsers(userResults);
 };
-getData();
+getData(1);
 
 const displayUsers = function (userResults) {
   //Clear randomFolks element
@@ -28,3 +30,8 @@ const displayUsers = function (userResults) {
     randomFolks.append(userDiv);
   }
 };
+
+selectUserNumber.addEventListener("change", function (e) {
+  const numUsers = e.target.value;
+  getData(numUsers);
+});
